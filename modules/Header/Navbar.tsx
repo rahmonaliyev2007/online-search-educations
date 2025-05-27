b9f07cf0179b9a0587fbe1612168a36ac8fd73ae
Abdulaziz Raxmonaliyev <rahmonaliyev2007@icloud.com>
2025-05-27 18:10:10 +0500

    page transition added

--- modules/Header/Navbar.tsx
+++ modules/Header/Navbar.tsx
@@ -1,13 +1,13 @@
 "use client"
 import { LocationIcon } from '@/assets/icons'
 import { useTranslations } from 'next-intl'
-import Link from 'next/link'
 import React, { useContext, useState } from 'react'
 import { getCookie } from 'cookies-next'
 import { ThemeContext } from '@/context/ThemeProvider'
 import { Moon, Sun } from 'lucide-react'
 import ChangeLang from './ChangeLang'
 import Logo from '@/public/images/logo'
+import TLink from '@/utils/TransitionLink'
 
 function Navbar() {
   const t = useTranslations("HeaderTop");
@@ -19,10 +19,10 @@ function Navbar() {
       <div className='containers flex justify-between items-center'>
         <div className='logo'><Logo/></div>
         <div className='flex items-center gap-[28px]'>
-          <Link href={'/'} className={`flex items-center gap-[10px] duration-300 ${theme === 'light' ? 'text-[#545D6A] hover:text-[#134E9B]' :'text-[#929599] hover:text-[#F8FAFC]'}`}><span>{t('home')}</span></Link>
-          <Link href={'/pages/aboutus'} className={`${theme === "light" ? 'text-[#545D6A] hover:text-[#134E9B]' : 'text-[#929599] hover:text-[#F8FAFC]'} duration-300 `}> <span>{t('aboutUs')}</span></Link>
-          <Link href={'/'} className={`${theme === "light" ? 'text-[#545D6A] hover:text-[#134E9B]' : 'text-[#929599] hover:text-[#F8FAFC]'} duration-300`}> <span>{t('resourses')}</span></Link>
-          <Link href={'/'} className={`${theme === "light" ? 'text-[#545D6A] hover:text-[#134E9B]' : 'text-[#929599] hover:text-[#F8FAFC]'} duration-300`}> <span>{t('liked')}</span></Link>
+          <TLink href={'/'} className={`flex items-center gap-[10px] duration-300 ${theme === 'light' ? 'text-[#545D6A] hover:text-[#134E9B]' :'text-[#929599] hover:text-[#F8FAFC]'}`}><span>{t('home')}</span></TLink>
+          <TLink href={'/pages/aboutus'} className={`${theme === "light" ? 'text-[#545D6A] hover:text-[#134E9B]' : 'text-[#929599] hover:text-[#F8FAFC]'} duration-300 `}> <span>{t('aboutUs')}</span></TLink>
+          <TLink href={'/'} className={`${theme === "light" ? 'text-[#545D6A] hover:text-[#134E9B]' : 'text-[#929599] hover:text-[#F8FAFC]'} duration-300`}> <span>{t('resourses')}</span></TLink>
+          <TLink href={'/'} className={`${theme === "light" ? 'text-[#545D6A] hover:text-[#134E9B]' : 'text-[#929599] hover:text-[#F8FAFC]'} duration-300`}> <span>{t('liked')}</span></TLink>
         </div>
         <div className='flex justify-end items-center gap-[18px]'>
           <ChangeLang/>
---
+++ utils/TransitionLink.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { Link} from '@/i18n/navigation'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+interface Props {
+    children: React.ReactNode,
+    href: string,
+    className: string
+}
+
+function sleep (ms: number) {
+  return new Promise(resolve =>
+    setTimeout(resolve, ms)
+    )
+}
+const TLink = ({children, href, className}: Props) => {
+    const router = useRouter();
+    
+    const handleTransition = async (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        e.preventDefault(); 
+
+        const body = document.querySelector('body');
+        body?.classList.add('page-transition');
+        await sleep(500);
+        router.push(href);
+        body?.classList.remove('page-transition');
+    }
+  return (
+    <Link onClick={handleTransition} href={href} className={className}>{children}</Link>
+  )
+}
+
+export default TLink
